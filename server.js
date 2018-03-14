753e479e943350656894a89e01918e19d4b4af76
Jim Nilsson <jimnils@hotmail.com>
2018-03-14 21:04:26 +0100

    route params and index html file

--- server.js
+++ server.js
@@ -7,27 +7,28 @@ const svenskafans = require('./feeds/svenskafans.js');
 const facebook = require('./feeds/facebook.js');
 const hammarbyfotboll = require('./feeds/hammarbyfotboll.js');
 
+const feeds = {
+  twitter,
+  svenskafans,
+  facebook,
+  hammarbyfotboll
+};
+
 app.use(cors());
 
-app.get('/', (req, res) => res.send('<div>Hello World!</div>'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.get('/feed', (req, res) => feed(req, res));
 
-app.get('/svenskafans/feed', (req, res) => svenskafans()
+//TODO: Try and figure out a way to get rid of this duplicate code.
+//https://expressjs.com/en/guide/routing.html#route-parameters
+app.get('/:source/feed', (req, res) => 
+  typeof feeds[req.params.source] === 'function' 
+  ? feeds[req.params.source]()
   .then(feed => res.json(feed))
-  .catch(err => console.log(err)));
-
-app.get('/twitter/feed', (req, res) => twitter()
-  .then(feed => res.json(feed))
-  .catch(err => console.log(err)));
-
-app.get('/facebook/feed', (req, res) => facebook()
-  .then(feed => res.json(feed))
-  .catch(err => console.log({err})));
-
-app.get('/hammarbyfotboll/feed', (req, res) => hammarbyfotboll()
-  .then(feed => res.json(feed))
-  .catch(err => console.log(err)));
+  .catch(err => res.status(500).send(err))
+  : res.status(404).send('Bad Request')
+);
 
 app.listen(process.env.PORT || 8080, () => 
   console.log('Example app listening on port ' + process.env.PORT || 8080))
